3dddafd64a4baaeb4a0d1ef1f04f176a850b35d7
Dale Fitzgerald <dale.fitzgerald@jdplc.com>
2022-05-05 22:11:48 +0100

    hid mongodb secrets

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-import dotenv from 'dotenv'
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const path = require('path');
-
-dotenv.config();
 
 const app = express();
 
